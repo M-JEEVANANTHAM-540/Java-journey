6003acb38b1ea0c0be319ca0527b72954c94e5b1
M-JEEVANANTHAM-540 <mjeevanantham540@gmail.com>
2026-06-30 11:15:03 +0530

    Checking purpose

--- Basics/Find even or odd.java
+++
@@ -1,5 +0,0 @@
-public class Find even or odd{
-    public static void main(string[] args){
-        System.out.println("Hello world");
-    }
-}
--- Basics/Hello.java
+++ Basics/Hello.java
@@ -1,5 +1,3 @@
-package Basics;
-
 public class Hello{
     public static void main(String[] args){
         System.out.println("Hello world");
