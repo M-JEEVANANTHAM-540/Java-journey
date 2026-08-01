714549506f71e40cb2fc95dfb98ae9ceda04da46
M-JEEVANANTHAM-540 <mjeevanantham540@gmail.com>
2026-08-02 02:01:53 +0530

    Meaningful message

---
+++ Array Data structure/DSA/max_element_in_array.java
@@ -0,0 +1,14 @@
+//Find the maximum element in a array
+public class Max_element_in_array{
+    public static void main(String args[]){
+        int array[]={5,4,8,6,3,8};
+        int max=array[0];
+        for (int i=0;i<array.length;i+=1){
+            
+            if (array[i]>=max){
+                max=array[i];
+            }
+        }
+        System.out.println("The maximum number in the array is "+max);
+    }
+}
---
+++ Basics/sub.java
@@ -0,0 +1,3 @@
+Integer.MAX_VALUE;   // 2147483647
+int min = Integer.MIN_VALUE;   // 2147483647
+int min = Integer.MIN_VALUE;
