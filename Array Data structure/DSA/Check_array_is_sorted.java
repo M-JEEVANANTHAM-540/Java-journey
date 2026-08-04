314f447dfa1798d25ccdd5d2ea3bd114d91b163c
M-JEEVANANTHAM-540 <mjeevanantham540@gmail.com>
2026-08-05 04:58:25 +0530

    Day 3-Check_if_an_array_is_sorted

---
+++ Array Data structure/DSA/Check_array_is_sorted.java
@@ -0,0 +1,19 @@
+public class Check_array_is_sorted {
+    public static void main(String args[]){
+        int arr[]={1,2,9,4};
+        boolean sorted=true;
+        for (int i=0;i<arr.length-1;i++){
+            if (arr[i]>arr[i+1]){
+                sorted=false;
+                break;
+            }
+        }
+        if (sorted){
+            System.out.println("The given array is sorted");
+        }
+        else{
+            System.out.println("The given array is not sorted");
+        }
+        
+    }
+}
