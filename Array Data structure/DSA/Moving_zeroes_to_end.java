0ed0b7d8891ea80c0281e4f53b93c43ac2c07cbf
M-JEEVANANTHAM-540 <mjeevanantham540@gmail.com>
2026-08-10 01:19:46 +0530

    Day 8: Move Zeroes to End

---
+++ Array Data structure/DSA/Moving_zeroes_to_end.java
@@ -0,0 +1,31 @@
+//[0,0,0,1,2]
+//1,2,3,0,0]
+//[0,1,0,2,0,3]
+//1]
+//[0]
+//[1,2,3]
+//[]
+
+
+
+
+
+public class Moving_zeroes_to_end {
+    public static void main(String args[]){
+        int arr[]={0,0,0,1,2};
+        int left=0;
+        int right=0;
+        while (right<arr.length){
+            if (arr[right]!=0){
+                arr[left]=arr[right];
+                right++;
+                left++;
+            }
+            else{
+                right++;
+            }}
+            for (int i=left;i<arr.length;i++){
+                arr[i]=0;}
+            for (int j=0;j<arr.length;j++){
+                System.out.print(arr[j]+" "); }}}
+
