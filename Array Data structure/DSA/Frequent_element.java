8cdc25e9ecd41177eaa93f5540073f118ac79d9a
M-JEEVANANTHAM-540 <mjeevanantham540@gmail.com>
2026-08-18 23:15:19 +0530

    Day 14: Most Frequent Element

--- Array Data structure/DSA/Frequent_element.java
+++ Array Data structure/DSA/Frequent_element.java
@@ -1,22 +1,21 @@
 public class Frequent_element {
     public static void main(String args[]){
-        int arr[]={1,1,22,2,2,3,2,4,5};
+        int arr[]={1,2,3,3,4,5};
         int most_frequent_element=arr[0];
         int m_count=0;
         for (int num:arr){
             int c_count=0;
             for (int val:arr){
                 if (num==val){
-                    c_count++;
+                    c_count++; }
                 }
-            }
             if(c_count>m_count){
                 m_count=c_count;
                 most_frequent_element=num;
             }
-
-            
         }
-        System.out.printf("The most frequent element is %d",most_frequent_element);
-    }
-}
+        if(m_count==1){
+            System.out.println(-1);
+        }
+        else{System.out.printf("The most frequent element in the array is %d and it appears %d times",most_frequent_element,m_count);} }}
+    
